76ef6ea087fda2b4e273470c507c6fa12b724a5b
fernandopal <f.pal.moreno@gmail.com>
2021-01-05 20:12:17 +0100

    Added try/catch exceptions and removed in-discord logs related code until I implement it in a better way
    Signed-off-by: fernandopal <f.pal.moreno@gmail.com>

--- src/main/java/es/fernandopal/yato/util/MessageUtil.java
+++ src/main/java/es/fernandopal/yato/util/MessageUtil.java
@@ -5,20 +5,23 @@ import java.awt.Color;
 import club.minnced.discord.webhook.WebhookClient;
 import club.minnced.discord.webhook.WebhookClientBuilder;
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
+import es.fernandopal.yato.listeners.ConnectionListeners;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MessageUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MessageUtil.class);
+
 	public void sendError(TextChannel tc, String msg) {
 		this.send(tc, msg, Color.RED);
 	}
-	
 	public void sendWarn(TextChannel tc, String msg) {
 		this.send(tc, msg, Color.YELLOW);
 	}
-	
 	public void sendOk(TextChannel tc, String msg) {
 		this.send(tc, msg, Color.GREEN);
 	}
@@ -26,13 +29,13 @@ public class MessageUtil {
 	public void sendPm(PrivateChannel pc, String msg, Color color) {
 		try {
 			pc.sendMessage(new EmbedBuilder().setColor(color).setDescription(msg).build()).queue();
-		} catch(Exception e) { }
+		} catch(Exception e) { LOGGER.error("Error trying to send a private message:"); e.printStackTrace(); }
 	}
 	
 	public void send(TextChannel tc, String msg, Color color) {
 		try {
 			tc.sendMessage(new EmbedBuilder().setColor(color).setDescription(msg).build()).queue();
-		} catch(Exception e) { }
+		} catch(Exception e) { LOGGER.error("Error trying to send a message:"); e.printStackTrace();  }
 	}
 	
 	public void send(TextChannel tc, String msg, Color color, boolean selfDelete) {
@@ -42,34 +45,35 @@ public class MessageUtil {
 			} else {
 				tc.sendMessage(new EmbedBuilder().setColor(color).setDescription(msg).build()).queue();
 			}
-		} catch(Exception e) { }
+		} catch(Exception e) { LOGGER.error("Error trying to send a message:"); e.printStackTrace();  }
 	}
 
-	public void sendWebhookEmbed(@NotNull WebhookEmbedBuilder content, String webhookUrl) {
-		WebhookClient client;
-		WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
-		builder.setThreadFactory((job) -> {
-			Thread thread = new Thread(job);
-			thread.setName("Webhook-Thread");
-			thread.setDaemon(true);
-			return thread;
-		});
+//	TODO: fix in-discord logs
+//	public void sendWebhookEmbed(@NotNull WebhookEmbedBuilder content, String webhookUrl) {
+//		WebhookClient client;
+//		WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
+//		builder.setThreadFactory((job) -> {
+//			Thread thread = new Thread(job);
+//			thread.setName("Webhook-Thread");
+//			thread.setDaemon(true);
+//			return thread;
+//		});
+//
+//		client = builder.build();
+//		client.send(content.build());
+//	}
 
-		client = builder.build();
-		client.send(content.build());
-	}
-
-	public void sendWebhookMsg(@NotNull String content, String webhookUrl) {
-		WebhookClient client;
-		WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
-		builder.setThreadFactory((job) -> {
-			Thread thread = new Thread(job);
-			thread.setName("Webhook-Thread");
-			thread.setDaemon(true);
-			return thread;
-		});
-
-		client = builder.build();
-		client.send(content);
-	}
+//	public void sendWebhookMsg(@NotNull String content, String webhookUrl) {
+//		WebhookClient client;
+//		WebhookClientBuilder builder = new WebhookClientBuilder(webhookUrl);
+//		builder.setThreadFactory((job) -> {
+//			Thread thread = new Thread(job);
+//			thread.setName("Webhook-Thread");
+//			thread.setDaemon(true);
+//			return thread;
+//		});
+//
+//		client = builder.build();
+//		client.send(content);
+//	}
 }
